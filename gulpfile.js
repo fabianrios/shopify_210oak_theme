d7d7e4c77cdc6212d3ca9563734355c92ef658f4
fabianrios <faben02@gmail.com>
2016-04-26 10:48:30 -0500

    basic with liveroload

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
+var livereload = require('gulp-livereload');
+var connect = require('gulp-connect')
+
 
 var sassPaths = [
   'bower_components/foundation-sites/scss',
   'bower_components/motion-ui/src'
 ];
+
+gulp.task('webserver', function() {
+  connect.server({
+      livereload: true
+    });
+});
 
 gulp.task('sass', function() {
   return gulp.src('scss/app.scss')
@@ -15,9 +24,10 @@ gulp.task('sass', function() {
     .pipe($.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('css'))
+    .pipe(connect.reload());
 });
 
-gulp.task('default', ['sass'], function() {
+gulp.task('default', ['sass','webserver'], function() {
   gulp.watch(['scss/**/*.scss'], ['sass']);
 });
